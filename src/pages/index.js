8dd142493c47c0a987df659b3ea458998abaf0bd
Alson Shareef <alson.shareef@gmail.com>
2019-08-09 14:40:54 +0800

    Added all section components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Intro from '../components/Intro/Intro';
+import About from '../components/About/About';
+import Projects from '../components/Projects/Projects';
+import Skills from '../components/Skills/Skills';
+import Contacts from '../components/Contacts/Contacts';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <Layout>
+        <SEO title="Home" />
+        <Intro />
+        <About />
+        <Projects />
+        <Skills />
+        <Contacts />
+    </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
