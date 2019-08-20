cd0b632b6fb506a3f8f82fc1f35dfb46d0ec2e4a
Alson Shareef <alson.shareef@gmail.com>
2019-08-20 18:04:01 +0800

    Added HTML for welcome banner to be render on initial website load.

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Intro.css';
 
 const Intro = () => {
-    return <div>Intro</div>;
+    return (
+        <section id="welcomeSection">
+            <h1>Hi, I'm Alson</h1>
+            <p>I'm a Fullstack JavaScript Developer</p>
+        </section>
+    );
 };
 
 export default Intro;
