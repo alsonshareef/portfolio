04c7fdaa2a838344b5733208b721b8edd8976e5e
Alson Shareef <alson.shareef@gmail.com>
2019-08-21 18:43:57 +0800

    Removed unnecessary ID

--- src/components/Sections/Intro/Intro.js
+++ src/components/Sections/Intro/Intro.js
@@ -3,7 +3,7 @@ import './Intro.css';
 
 const Intro = () => {
     return (
-        <section id="welcomeSection">
+        <section>
             <h1>Hi, I'm Alson</h1>
             <p>I'm a Fullstack JavaScript Developer</p>
         </section>
