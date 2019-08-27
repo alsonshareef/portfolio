682a62016bd15bfbefe4f058d174126f0b22dada
alsonshareef <alson.shareef@gmail.com>
2019-08-27 17:40:22 +0800

    Changed filename and added Github and Linkedin links using respective react icons.

---
+++ src/components/Sections/About/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import aboutStyles from './about.module.scss';
+
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const About = () => {
+    return (
+        <div>
+            About
+            <a
+                href="https://github.com/alsonshareef"
+                target="_blank"
+                className={aboutStyles.icon}
+            >
+                <FaGithub />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/alsonshareef/"
+                target="_blank"
+                className={aboutStyles.icon}
+            >
+                <FaLinkedin />
+            </a>
+        </div>
+    );
+};
+
+export default About;
