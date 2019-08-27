9379e7b807e9facf0c2be78a0104bf89a0b53e74
alsonshareef <alson.shareef@gmail.com>
2019-08-27 17:36:36 +0800

    Changed relative paths for Banner and About components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../styles/index.scss';
 
 import Layout from '../components/UI/Layout/layout';
-import Banner from '../components/Sections/Banner/Banner';
-import About from '../components/Sections/About/About';
+import Banner from '../components/Sections/Banner/banner';
+import About from '../components/Sections/About/about';
 import Projects from '../components/Sections/Projects/Projects';
 import Skills from '../components/Sections/Skills/Skills';
 import Contacts from '../components/Sections/Contacts/Contacts';
