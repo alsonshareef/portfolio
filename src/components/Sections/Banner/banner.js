f004b95b9dd879ca8bb029b47652409b362f6da5
alsonshareef <alson.shareef@gmail.com>
2019-08-28 17:33:58 +0800

    Wrapped banner content in a div incase styles need to be made to them specifically.

--- src/components/Sections/Banner/banner.js
+++ src/components/Sections/Banner/banner.js
@@ -4,13 +4,15 @@ import bannerStyles from './banner.module.scss';
 const Banner = () => {
     return (
         <div className={bannerStyles.banner}>
-            <h1 className={bannerStyles.bannerTitle}>
-                Hi, my name is <span>Alson Shareef</span>
-            </h1>
-            <h2 className={bannerStyles.bannerSubTitle}>
-                I'm a Fullstack Web Developer with a passion for the JavaScript
-                ecosystem.
-            </h2>
+            <div>
+                <h1 className={bannerStyles.bannerTitle}>
+                    Hi, my name is <span>Alson Shareef</span>
+                </h1>
+                <h2 className={bannerStyles.bannerSubTitle}>
+                    I'm a Fullstack Web Developer with a passion for the
+                    JavaScript ecosystem.
+                </h2>
+            </div>
         </div>
     );
 };
