ab679c81e757d50fca00d3c876db586a166abfbf
alsonshareef <alson.shareef@gmail.com>
2019-09-04 18:21:24 +0800

    Added a heading container with respective classe for positioning bannerHeading and changed the JSX a little.

--- src/components/Sections/Banner/banner.js
+++ src/components/Sections/Banner/banner.js
@@ -4,14 +4,16 @@ import bannerStyles from './banner.module.scss';
 const Banner = () => {
     return (
         <div className={bannerStyles.banner}>
-            <div>
-                <h1 className={bannerStyles.bannerTitle}>
-                    Hi, my name is <span>Alson Shareef</span>
+            <div className={bannerStyles.container}>
+                <h1 className={bannerStyles.bannerHeading}>
+                    <p>
+                        Hi, my name is <span>Alson Shareef</span>
+                    </p>
+                    <p className={bannerStyles.text}>
+                        I'm a Fullstack Web Developer with a passion for the
+                        JavaScript ecosystem.
+                    </p>
                 </h1>
-                <h2 className={bannerStyles.bannerSubTitle}>
-                    I'm a Fullstack Web Developer with a passion for the
-                    JavaScript ecosystem.
-                </h2>
             </div>
         </div>
     );
