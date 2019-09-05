667df42922475b00891d66dad6b830d9e19965fd
alsonshareef <alson.shareef@gmail.com>
2019-09-05 13:44:06 +0800

    New relative paths.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import '../styles/index.scss';
 import Layout from '../components/UI/Layout/layout';
 import Banner from '../components/Sections/Banner/banner';
 import About from '../components/Sections/About/about';
-import Projects from '../components/Sections/Projects/Projects';
-import Skills from '../components/Sections/Skills/Skills';
-import Contacts from '../components/Sections/Contacts/Contacts';
+import Projects from '../components/Sections/Projects/projects';
+import Skills from '../components/Sections/Skills/skills';
+import Contacts from '../components/Sections/Contacts/contacts';
 
 const IndexPage = () => (
     <Layout>
