c1f0b66de1222c7b3ca48abd221d4c51335d21cf
alsonshareef <alson.shareef@gmail.com>
2019-09-05 17:59:49 +0800

    Import skillsStyles and made component return a section element.

--- src/components/Sections/Skills/skills.js
+++ src/components/Sections/Skills/skills.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import skillsStyles from './skills.module.scss';
 
 const Skills = () => {
-    return <section>Skills</section>;
+    return (
+        <section className={skillsStyles.skillsPage}>
+            <h2>Skills</h2>
+        </section>
+    );
 };
 
 export default Skills;
