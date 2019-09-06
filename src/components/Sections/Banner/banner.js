af5008b30dc95539fecb296bc8a36eb8d8179d43
alsonshareef <alson.shareef@gmail.com>
2019-09-06 14:43:04 +0800

    Used react-spring to create a fadeInLeft animation for both paragraphs within the h1. Text paragraph has a delay to start animation 2 seconds after heading paragraph has.

--- src/components/Sections/Banner/banner.js
+++ src/components/Sections/Banner/banner.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import bannerStyles from './banner.module.scss';
 
+import { useSpring, animated } from 'react-spring';
+
 const Banner = () => {
+    const fadeInLeft = delay =>
+        useSpring({
+            config: {
+                duration: 1250,
+            },
+
+            from: {
+                opacity: 0,
+                transform: 'translateX(-40px)',
+            },
+
+            opacity: 1,
+            transform: 'translateX(0px)',
+            delay: delay,
+        });
+
     return (
         <div className={bannerStyles.banner}>
             <div className={bannerStyles.container}>
                 <h1 className={bannerStyles.bannerHeading}>
-                    <p>
+                    <animated.p style={fadeInLeft(0)}>
                         Hi, my name is <span>Alson Shareef</span>
-                    </p>
-                    <p className={bannerStyles.text}>
+                    </animated.p>
+                    <animated.p
+                        className={bannerStyles.text}
+                        style={fadeInLeft(2000)}
+                    >
                         I'm a Fullstack Web Developer with a passion for the
                         JavaScript ecosystem.
-                    </p>
+                    </animated.p>
                 </h1>
             </div>
         </div>
