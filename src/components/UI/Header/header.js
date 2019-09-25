7d1724ef463e0015c8f4460e21526d273a303f99
Alson Shareef <alson.shareef@gmail.com>
2019-09-25 18:46:35 +0800

    Refactored entire navbar to implement Bulma classes.

--- src/components/UI/Header/header.js
+++ src/components/UI/Header/header.js
@@ -1,40 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import headerStyles from './header.module.scss';
 
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-const Header = () => (
-    <header>
-        <nav className={headerStyles.navbar}>
-            <ul className={headerStyles.navbarItems}>
-                <li>
-                    <AnchorLink offset={() => 900} href="#top">
-                        Home
-                    </AnchorLink>
-                </li>
-                <li>
-                    <AnchorLink offset={() => 50} href="#about">
-                        About
-                    </AnchorLink>
-                </li>
-                <li>
-                    <AnchorLink offset={() => 50} href="#projects">
-                        Projects
-                    </AnchorLink>
-                </li>
-                <li>
-                    <AnchorLink offset={() => 50} href="#skills">
-                        Skills
-                    </AnchorLink>
-                </li>
-                <li>
-                    <AnchorLink offset={() => 50} href="#contacts">
-                        Contacts
-                    </AnchorLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
-);
+const Header = () => {
+    // Tracks active state of navbar-burger button.
+    const [active, toggleActive] = useState(false);
+
+    return (
+        <header>
+            <nav
+                className={[
+                    'navbar',
+                    'is-fixed-top',
+                    headerStyles.navbar__custom,
+                ].join(' ')}
+            >
+                <div className="navbar-brand">
+                    <a className="navbar-item is-size-4" href="#top">
+                        Alson Shareef
+                    </a>
+
+                    <button
+                        onClick={() => toggleActive(!active)}
+                        className={
+                            active ? 'navbar-burger is-active' : 'navbar-burger'
+                        }
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navMenu"
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </button>
+                </div>
+
+                <div
+                    id="navMenu"
+                    className={active ? 'navbar-menu is-active' : 'navbar-menu'}
+                >
+                    <div className="navbar-end is-size-6">
+                        <AnchorLink
+                            className="navbar-item is-tab is-hoverable"
+                            offset={() => 900}
+                            href="#top"
+                        >
+                            Home
+                        </AnchorLink>
+
+                        <AnchorLink
+                            className="navbar-item is-tab"
+                            offset={() => 50}
+                            href="#about"
+                        >
+                            About
+                        </AnchorLink>
+
+                        <AnchorLink
+                            className="navbar-item is-tab"
+                            offset={() => 50}
+                            href="#projects"
+                        >
+                            Projects
+                        </AnchorLink>
+
+                        <AnchorLink
+                            className="navbar-item is-tab"
+                            offset={() => 50}
+                            href="#skills"
+                        >
+                            Skills
+                        </AnchorLink>
+
+                        <AnchorLink
+                            className="navbar-item is-tab"
+                            offset={() => 50}
+                            href="#contacts"
+                        >
+                            Contacts
+                        </AnchorLink>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
