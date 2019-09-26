9b465d79f6e18de5711b69af644209f9f58b7109
Alson Shareef <alson.shareef@gmail.com>
2019-09-26 18:54:29 +0800

    Changed indexPage component to class-based to hold a defaultConfig property for scrollreveal which can be passed to all components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/index.scss';
 
 import Layout from '../components/UI/Layout/layout';
@@ -8,15 +8,28 @@ import Projects from '../components/Sections/Projects/projects';
 import Skills from '../components/Sections/Skills/skills';
 import Contacts from '../components/Sections/Contacts/contacts';
 
-const IndexPage = () => (
-    <Layout>
-        <div id="top" />
-        <Banner />
-        <About />
-        <Projects />
-        <Skills />
-        <Contacts />
-    </Layout>
-);
+class IndexPage extends Component {
+    // Default scrollReveal config properties.
+    srDefaultConfig = {
+        easing: 'cubic-bezier(0.5, 0, 0, 1)',
+        distance: '30px',
+        duration: 1000,
+        desktop: true,
+        mobile: true,
+    };
+
+    render() {
+        return (
+            <Layout>
+                <div id="top" />
+                <Banner srDefaultConfig={this.srDefaultConfig} />
+                <About srDefaultConfig={this.srDefaultConfig} />
+                <Projects srDefaultConfig={this.srDefaultConfig} />
+                <Skills srDefaultConfig={this.srDefaultConfig} />
+                <Contacts srDefaultConfig={this.srDefaultConfig} />
+            </Layout>
+        );
+    }
+}
 
 export default IndexPage;
