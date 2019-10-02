4b83c1e7ecb51e25b7f0a5c3c5063e6166b9d3b1
alsonshareef <alson.shareef@gmail.com>
2019-10-02 19:01:27 +0800

    Changed component to class-based to utilise lifecycle hooks for scrollReveal library useage. Also applied Bulma classes primarily for layout and responsiveness needs and some custom styles.

--- src/components/Sections/About/about.js
+++ src/components/Sections/About/about.js
@@ -1,68 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 import aboutStyles from './about.module.scss';
 
+import sr from '../../../scrollReveal';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-const About = () => {
-    return (
-        <section id="about" className={aboutStyles.about__page}>
-            <h2>About Me</h2>
-            <div className={aboutStyles.container}>
-                <div className={aboutStyles.sidebar}>
-                    {/* Replace img element with gatsby-image. */}
-                    <img
-                        className={aboutStyles.sidebar__image}
-                        src="https://i.pravatar.cc/300"
-                        alt=""
-                    ></img>
-                    <ul className={aboutStyles.sidebar__socials}>
-                        <li className={aboutStyles.sidebar__socials__items}>
-                            <a
-                                href="https://github.com/alsonshareef"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={
-                                    aboutStyles.sidebar__socials__items__link
-                                }
-                            >
-                                <FaGithub />
-                            </a>
-                        </li>
-                        <li className={aboutStyles.sidebar__socials__items}>
-                            <a
-                                href="https://www.linkedin.com/in/alsonshareef/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={
-                                    aboutStyles.sidebar__socials__items__link
-                                }
-                            >
-                                <FaLinkedin />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+class About extends Component {
+    componentDidMount() {
+        const h3Config = {
+            ...this.props.srDefaultConfig,
+            delay: 500,
+            distance: '0px',
+            origin: 'bottom',
+        };
 
-                <div className={aboutStyles.main}>
-                    <p className={aboutStyles.main__info}>
-                        I'm a passionate Full-Stack Web Developer focused on
-                        consistent learning and pushing my boundaries to become
-                        a better problem-solver.
-                    </p>
-                    <p className={aboutStyles.main__info}>
-                        I have been primarly focused on learning the JavaScript
-                        ecosystem and how to utilise these technologies to build
-                        clean, perfomant front-ends and robust back-ends.
-                    </p>
-                    <p className={aboutStyles.main__info}>
-                        In the sections below you will see the technologies I
-                        have been focused on learning as well as the projects I
-                        have built in order to properly understand them.
-                    </p>
+        sr.reveal(this.refs.abouth3, h3Config);
+    }
+
+    render() {
+        return (
+            <section
+                id="about"
+                className={['section', aboutStyles.about__page].join(' ')}
+            >
+                <div className="container">
+                    <h3
+                        className={['title', aboutStyles.title].join(' ')}
+                        ref="abouth3"
+                    >
+                        About Me
+                    </h3>
+                    <div className="columns">
+                        <div className="column has-text-centered">
+                            {/* Replace img element with gatsby-image. */}
+                            <img
+                                src="https://i.pravatar.cc/300"
+                                alt="placeholder"
+                            ></img>
+                            <ul className={[aboutStyles.socials].join(' ')}>
+                                <li className="button">
+                                    <a
+                                        href="https://github.com/alsonshareef"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <FaGithub />
+                                    </a>
+                                </li>
+                                <li className="button">
+                                    <a
+                                        href="https://www.linkedin.com/in/alsonshareef/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <FaLinkedin />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div
+                            className={[
+                                'column',
+                                'has-text-centered-mobile',
+                                aboutStyles.textColumn,
+                            ].join(' ')}
+                        >
+                            <p>
+                                I'm a passionate Full-Stack Web Developer
+                                focused on consistent learning and pushing my
+                                boundaries to become a better problem-solver.
+                            </p>
+                            <p>
+                                I have been primarly focused on learning the
+                                JavaScript ecosystem and how to utilise these
+                                technologies to build clean, perfomant
+                                front-ends and robust back-ends.
+                            </p>
+                            <p>
+                                In the sections below you will see the
+                                technologies I have been focused on learning as
+                                well as the projects I have built in order to
+                                properly understand them.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
-    );
-};
+            </section>
+        );
+    }
+}
 
 export default About;
