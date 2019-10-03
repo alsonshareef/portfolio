5be7ad1efa969d533584cbaba536b819adb28b92
alsonshareef <alson.shareef@gmail.com>
2019-10-03 17:18:51 +0800

    Applied scroll-reveal library through useage of lifecycle hooks and added bulma classes to heading texts for responsive font-sizing.

--- src/components/Sections/Banner/banner.js
+++ src/components/Sections/Banner/banner.js
@@ -5,7 +5,7 @@ import sr from '../../../scrollReveal';
 
 class Banner extends Component {
     // Applies scroll reveal functionality to heading after mounting.
-    componentDidMount = () => {
+    componentDidMount() {
         // Scroll reveal config for h1
         const h1Config = {
             ...this.props.srDefaultConfig,
@@ -14,7 +14,7 @@ class Banner extends Component {
             origin: window.innerWidth > 768 ? 'left' : 'bottom',
         };
 
-        sr.reveal('.title', h1Config);
+        sr.reveal(this.refs.bannerh1, h1Config);
 
         // Scroll reveal config for h2
         const h2config = {
@@ -24,12 +24,12 @@ class Banner extends Component {
             origin: window.innerWidth > 768 ? 'left' : 'bottom',
         };
 
-        sr.reveal('.subtitle', h2config);
-    };
+        sr.reveal(this.refs.bannerh2, h2config);
+    }
 
     render() {
         return (
-            <div
+            <section
                 id="banner"
                 className={['hero', 'is-fullheight', bannerStyles.banner].join(
                     ' '
@@ -37,20 +37,26 @@ class Banner extends Component {
             >
                 <div className={'hero-body'}>
                     <div className="container has-text-centered-mobile">
-                        <h1 className="title is-size-3-mobile is-size-2-tablet is-size-1-desktop">
+                        <h1
+                            className="title is-size-3-mobile is-size-2-tablet is-size-1-desktop"
+                            ref="bannerh1"
+                        >
                             Hi, my name is{' '}
                             <span className={bannerStyles.headingMain__name}>
                                 Alson Shareef
                             </span>
                         </h1>
 
-                        <h2 className="subtitle is-size-5-mobile is-size-4-tablet is-size-3-desktop">
+                        <h2
+                            className="subtitle is-size-5-mobile is-size-4-tablet is-size-3-desktop"
+                            ref="bannerh2"
+                        >
                             I enjoy problem-solving and building meaningful
                             solutions for the web.
                         </h2>
                     </div>
                 </div>
-            </div>
+            </section>
         );
     }
 }
