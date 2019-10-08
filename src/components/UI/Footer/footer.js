d236bcc2b1bbf3b7624f5dabd1f10ab6f061eab3
alsonshareef <alson.shareef@gmail.com>
2019-10-08 18:17:39 +0800

    Used graphql to access author meta-data to be displayed in footer text.

--- src/components/UI/Footer/footer.js
+++ src/components/UI/Footer/footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 // import footerStyles from './footer.module.scss';
 
 const footer = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    author
+                }
+            }
+        }
+    `);
+
     return (
         <footer>
-            <p>Footer</p>
+            <p>{`Created by ${data.site.siteMetadata.author}, ©️ 2019`}</p>
         </footer>
     );
 };
