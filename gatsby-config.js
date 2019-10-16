69f26c2ca755efa1aa0cc629d850ea45d306d867
alsonshareef <alson.shareef@gmail.com>
2019-10-16 20:08:40 +0800

    Applied transformer-sharp, sharp plugin and gatsby-source-filesystem for sourcing images from local file system using graphql.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
+const path = require('path');
+
 module.exports = {
     siteMetadata: {
         title: 'Portfolio',
         author: 'Alson Shareef',
     },
-    plugins: ['gatsby-plugin-sass', `gatsby-plugin-react-helmet`],
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: path.join(__dirname, `src`, `images`),
+            },
+        },
+        'gatsby-plugin-sass',
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+    ],
 };
