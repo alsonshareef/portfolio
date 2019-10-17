badbdc42145d7774530d45db61f4a46819051c22
alsonshareef <alson.shareef@gmail.com>
2019-10-17 19:07:41 +0800

    Refactored

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,19 @@
-const path = require('path');
-
 module.exports = {
     siteMetadata: {
         title: 'Portfolio',
         author: 'Alson Shareef',
     },
     plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: path.join(__dirname, `src`, `images`),
-            },
-        },
         'gatsby-plugin-sass',
         `gatsby-plugin-react-helmet`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
     ],
 };
