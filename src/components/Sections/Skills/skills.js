8c452daec4072df7860774d4bfaa22cf2f1966ab
alsonshareef <alson.shareef@gmail.com>
2019-10-19 19:37:53 +0800

    Create an array of components which create a skill item which contains gatsby images of skill logos

--- src/components/Sections/Skills/skills.js
+++ src/components/Sections/Skills/skills.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 import skillsStyles from './skills.module.scss';
 
-// import { graphql, StaticQuery } from 'gatsby';
-// import Img from 'gatsby-image';
-
-import reactLogo from '../../../images/react.svg';
-import nodeLogo from '../../../images/nodejs.png';
-import babelLogo from '../../../images/babel.svg';
-import npmLogo from '../../../images/npm.svg';
-import htmlLogo from '../../../images/html5.svg';
-import webpackLogo from '../../../images/webpack.png';
-import gatsbyLogo from '../../../images/gatsby.svg';
-import cssLogo from '../../../images/css.svg';
-import jsLogo from '../../../images/javascript.png';
-import expressLogo from '../../../images/express.png';
-import mongodbLogo from '../../../images/mongodb.png';
-// import postgresLogo from '../../../images/postgresql.png';
-import sassLogo from '../../../images/sass.png';
-import githubLogo from '../../../images/github.png';
-import graphqlLogo from '../../../images/graphql.png';
+import Img from 'gatsby-image';
 
 class Skills extends Component {
+    skillImageNodes = this.props.data;
+    skillItems = this.skillImageNodes.map(node => (
+        <div className="level-item">
+            <div className={[skillsStyles.img_container].join(' ')}>
+                <Img fluid={node.childImageSharp.fluid} />
+                <p>{node.name}</p>
+            </div>
+        </div>
+    ));
+
     render() {
         return (
             <section
@@ -41,30 +34,12 @@ class Skills extends Component {
                             Front-End
                         </h4>
                         <div className="level">
-                            <div className="level-item">
-                                <img src={htmlLogo} alt="html logo" />
-                                <p>HTML5</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={cssLogo} alt="html logo" />
-                                <p>CSS3</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={sassLogo} alt="sass logo" />
-                                <p>Sass</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={jsLogo} alt="javascript logo" />
-                                <p>JavaScript</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={reactLogo} alt="react logo" />
-                                <p>React</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={gatsbyLogo} alt="gatsby logo" />
-                                <p>Gatsby</p>
-                            </div>
+                            {this.skillItems[10]}
+                            {this.skillItems[8]}
+                            {this.skillItems[5]}
+                            {this.skillItems[3]}
+                            {this.skillItems[12]}
+                            {this.skillItems[9]}
                         </div>
                     </div>
                     <div className="box">
@@ -77,14 +52,8 @@ class Skills extends Component {
                             Back-End
                         </h4>
                         <div className="level">
-                            <div className="level-item">
-                                <img src={nodeLogo} alt="node logo" />
-                                <p>Node.js</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={expressLogo} alt="express logo" />
-                                <p>Express</p>
-                            </div>
+                            {this.skillItems[14]}
+                            {this.skillItems[0]}
                         </div>
                     </div>
                     <div className="box">
@@ -97,18 +66,9 @@ class Skills extends Component {
                             Databases / Tooling
                         </h4>
                         <div className="level">
-                            <div className="level-item">
-                                <img src={mongodbLogo} alt="mongodb logo" />
-                                <p>MongoDB</p>
-                            </div>
-                            <div className="level-item">
-                                {/* <img src={postgresLogo} alt="postgresql logo" /> */}
-                                <p>Postgres</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={graphqlLogo} alt="graphql logo" />
-                                <p>GraphQL</p>
-                            </div>
+                            {this.skillItems[4]}
+                            {this.skillItems[7]}
+                            {this.skillItems[2]}
                         </div>
                     </div>
                     <div className="box">
@@ -121,22 +81,10 @@ class Skills extends Component {
                             Build-Tools
                         </h4>
                         <div className="level">
-                            <div className="level-item">
-                                <img src={webpackLogo} alt="webpack logo" />
-                                <p>Webpack</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={babelLogo} alt="babel logo" />
-                                <p>Babel</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={npmLogo} alt="npm logo" />
-                                <p>npm</p>
-                            </div>
-                            <div className="level-item">
-                                <img src={githubLogo} alt="github logo" />
-                                <p>Git</p>
-                            </div>
+                            {this.skillItems[6]}
+                            {this.skillItems[13]}
+                            {this.skillItems[11]}
+                            {this.skillItems[1]}
                         </div>
                     </div>
                 </div>
