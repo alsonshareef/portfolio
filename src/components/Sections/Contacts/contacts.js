c2a0960263b783e9494ed9705914b16f7a3ee7b7
alsonshareef <alson.shareef@gmail.com>
2019-10-22 18:26:10 +0800

    Added base contact section structure with font awesome react icons for social links

--- src/components/Sections/Contacts/contacts.js
+++ src/components/Sections/Contacts/contacts.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import contactsStyles from './contacts.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 const Contacts = () => {
     return (
         <section
@@ -8,7 +12,45 @@ const Contacts = () => {
             id="contacts"
         >
             <div className="container">
-                <h3 className="title">Contacts</h3>
+                <h3 className={['title', contactsStyles.title].join(' ')}>
+                    Contacts
+                </h3>
+                <div className="contact content">
+                    <p>If you would like to reach out... </p>
+                    <a
+                        className={[
+                            'button is-large',
+                            contactsStyles.link,
+                        ].join(' ')}
+                        href="mailto:alson.shareef@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </a>
+                    <a
+                        className={[
+                            'button is-large',
+                            contactsStyles.link,
+                        ].join(' ')}
+                        href="https://github.com/alsonshareef"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                    <a
+                        className={[
+                            'button is-large',
+                            contactsStyles.link,
+                        ].join(' ')}
+                        href="https://www.linkedin.com/in/alsonshareef/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                </div>
             </div>
         </section>
     );
