340cc91db20ec423a12e2be21bbd9736109762aa
alsonshareef <alson.shareef@gmail.com>
2019-10-23 17:45:26 +0800

    Refactored structure to utilise bulma columns instead of levels to create a cleaner UI.

--- src/components/Sections/Skills/skills.js
+++ src/components/Sections/Skills/skills.js
@@ -4,15 +4,15 @@ import skillsStyles from './skills.module.scss';
 import Img from 'gatsby-image';
 
 class Skills extends Component {
-    // Array of components holding html for each skill item using data from graphql query.
+    // // Array of components holding html for each skill item using data from graphql query.
     skillItems = this.props.data.map(node => (
-        <div className={['level-item', skillsStyles.level_item].join(' ')}>
+        <div className={['box', skillsStyles.skill_item].join(' ')}>
             <div className={[skillsStyles.img_container].join(' ')}>
                 <Img fluid={node.childImageSharp.fluid} />
-                <p className="has-text-weight-semibold">
-                    {node.name === 'Node' ? `${node.name}.js` : node.name}
-                </p>
             </div>
+            <p className="has-text-weight-semibold">
+                {node.name === 'Node' ? `${node.name}.js` : node.name}
+            </p>
         </div>
     ));
 
@@ -26,67 +26,61 @@ class Skills extends Component {
                     Skills
                 </h3>
                 <div className="container">
-                    <div className="box">
-                        <h4
-                            className={[
-                                'subtitle is-4',
-                                skillsStyles.subtitle,
-                            ].join(' ')}
-                        >
-                            Front-End
-                        </h4>
-                        <div className="level">
-                            {this.skillItems[10]}
-                            {this.skillItems[8]}
-                            {this.skillItems[5]}
-                            {this.skillItems[3]}
-                            {this.skillItems[12]}
-                            {this.skillItems[9]}
-                        </div>
-                    </div>
-                    <div className="box">
-                        <h4
-                            className={[
-                                'subtitle is-4',
-                                skillsStyles.subtitle,
-                            ].join(' ')}
-                        >
-                            Back-End
-                        </h4>
-                        <div className="level">
-                            {this.skillItems[14]}
-                            {this.skillItems[0]}
-                        </div>
-                    </div>
-                    <div className="box">
-                        <h4
-                            className={[
-                                'subtitle is-4',
-                                skillsStyles.subtitle,
-                            ].join(' ')}
-                        >
-                            Databases / Tooling
-                        </h4>
-                        <div className="level">
-                            {this.skillItems[4]}
+                    <div className="columns">
+                        <div className="column">
+                            <h4
+                                className={[
+                                    'subtitle is-4',
+                                    skillsStyles.subtitle,
+                                ].join(' ')}
+                            >
+                                Front-End
+                            </h4>
                             {this.skillItems[7]}
                             {this.skillItems[2]}
-                        </div>
-                    </div>
-                    <div className="box">
-                        <h4
-                            className={[
-                                'subtitle is-4',
-                                skillsStyles.subtitle,
-                            ].join(' ')}
-                        >
-                            Build-Tools
-                        </h4>
-                        <div className="level">
-                            {this.skillItems[6]}
-                            {this.skillItems[13]}
+                            {this.skillItems[12]}
+                            {this.skillItems[8]}
                             {this.skillItems[11]}
+                            {this.skillItems[4]}
+                        </div>
+                        <div className="column">
+                            <h4
+                                className={[
+                                    'subtitle is-4',
+                                    skillsStyles.subtitle,
+                                ].join(' ')}
+                            >
+                                Back-End
+                            </h4>
+                            {this.skillItems[10]}
+                            {this.skillItems[3]}
+                        </div>
+                        <div className="column">
+                            <h4
+                                className={[
+                                    'subtitle is-4',
+                                    skillsStyles.subtitle,
+                                ].join(' ')}
+                            >
+                                Databases / Tooling
+                            </h4>
+                            {this.skillItems[9]}
+                            {this.skillItems[14]}
+                            {this.skillItems[6]}
+                        </div>
+                        <div className="column">
+                            <h4
+                                className={[
+                                    'subtitle is-4',
+                                    skillsStyles.subtitle,
+                                ].join(' ')}
+                            >
+                                Build-Tools
+                            </h4>
                             {this.skillItems[1]}
+                            {this.skillItems[0]}
+                            {this.skillItems[13]}
+                            {this.skillItems[5]}
                         </div>
                     </div>
                 </div>
