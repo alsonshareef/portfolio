f7a5cfc4b87035e812fd7c42ee7fc2ea8586c873
alsonshareef <alson.shareef@gmail.com>
2019-10-31 18:18:16 +0800

    Added scroll reveal to title and each project card. Also removed map method for creating project images.

--- src/components/Sections/Projects/projects.js
+++ src/components/Sections/Projects/projects.js
@@ -1,97 +1,159 @@
-import React from 'react';
+import React, { Component } from 'react';
 import projectStyles from './projects.module.scss';
 import Img from 'gatsby-image';
 
-const Projects = ({ data }) => {
-    // Array of gatsby image versions of project images queried from graphql
-    const projectImgs = data.map(node => (
-        <Img fluid={node.childImageSharp.fluid} />
-    ));
+class Projects extends Component {
+    componentDidMount() {
+        const titleConfig = {
+            ...this.props.defaultSR,
+            delay: 600,
+            origin: 'bottom',
+        };
 
-    return (
-        <section
-            className={['section', projectStyles.projects__page].join(' ')}
-            id="projects"
-        >
-            <div className="container">
-                <h3 className={['title', projectStyles.title].join(' ')}>
-                    Projects
-                </h3>
-                <div className="columns">
-                    <div className="column">
-                        <div className={['card', projectStyles.card].join(' ')}>
-                            <header className="card-header">
-                                <p className="card-header-title">Project 1</p>
-                            </header>
-                            <div className="card-content">
-                                {projectImgs[0]}
-                                <div className="content">
-                                    This is the description for Project 1.
+        const projectConfig = {
+            ...this.props.defaultSR,
+            delay: 1000,
+            origin: 'left',
+        };
+
+        import('../../../scrollReveal').then(module => {
+            // Title SR
+            module.default.reveal(this.refs.title, titleConfig);
+
+            // Project card SR
+            for (const refProp of Object.keys(this.refs)) {
+                if (refProp.includes('project')) {
+                    module.default.reveal(this.refs[refProp], projectConfig);
+                }
+            }
+        });
+    }
+
+    render() {
+        return (
+            <section
+                className={['section', projectStyles.projects__page].join(' ')}
+                id="projects"
+            >
+                <div className="container">
+                    <h3
+                        className={['title', projectStyles.title].join(' ')}
+                        ref="title"
+                    >
+                        Projects
+                    </h3>
+                    <div className="columns">
+                        <div className="column">
+                            <div
+                                className={['card', projectStyles.card].join(
+                                    ' '
+                                )}
+                                ref="project1"
+                            >
+                                <header className="card-header">
+                                    <p className="card-header-title">
+                                        Coming soon...
+                                    </p>
+                                </header>
+                                <div className="card-content">
+                                    <Img
+                                        fluid={
+                                            this.props.data[0].childImageSharp
+                                                .fluid
+                                        }
+                                    />
+                                    <div className="content">
+                                        -- project description --
+                                    </div>
+                                    <span className="tag">HTML</span>
+                                    <span className="tag">CSS</span>
+                                    <span className="tag">Javascript</span>
                                 </div>
-                                <span className="tag">HTML</span>
-                                <span className="tag">CSS</span>
-                                <span className="tag">Javascript</span>
+                                <footer className="card-footer">
+                                    <a href="/" className="card-footer-item">
+                                        Live Demo
+                                    </a>
+                                    <a href="/" className="card-footer-item">
+                                        Source Code
+                                    </a>
+                                </footer>
                             </div>
-                            <footer className="card-footer">
-                                <a href="/" className="card-footer-item">
-                                    Live Demo
-                                </a>
-                                <a href="/" className="card-footer-item">
-                                    Source Code
-                                </a>
-                            </footer>
+                            <div
+                                className={['card', projectStyles.card].join(
+                                    ' '
+                                )}
+                                ref="project2"
+                            >
+                                <header className="card-header">
+                                    <p className="card-header-title">
+                                        Coming soon...
+                                    </p>
+                                </header>
+                                <div className="card-content">
+                                    <div className="content">
+                                        <Img
+                                            fluid={
+                                                this.props.data[0]
+                                                    .childImageSharp.fluid
+                                            }
+                                        />
+                                        -- project description --
+                                    </div>
+                                    <span className="tag">Node.js</span>
+                                    <span className="tag">Express</span>
+                                    <span className="tag">MongoDB</span>
+                                </div>
+                                <footer className="card-footer">
+                                    <a href="/" className="card-footer-item">
+                                        Live Demo
+                                    </a>
+                                    <a href="/" className="card-footer-item">
+                                        Source Code
+                                    </a>
+                                </footer>
+                            </div>
                         </div>
-                        <div className={['card', projectStyles.card].join(' ')}>
-                            <header className="card-header">
-                                <p className="card-header-title">Project 3</p>
-                            </header>
-                            <div className="card-content">
-                                <div className="content">
-                                    {projectImgs[0]}
-                                    This is the description for Project 3.
+                        <div className="column">
+                            <div
+                                className={['card', projectStyles.card].join(
+                                    ' '
+                                )}
+                                ref="project3"
+                            >
+                                <header className="card-header">
+                                    <p className="card-header-title">
+                                        Coming soon...
+                                    </p>
+                                </header>
+                                <div className="card-content">
+                                    <div className="content">
+                                        <Img
+                                            fluid={
+                                                this.props.data[0]
+                                                    .childImageSharp.fluid
+                                            }
+                                        />
+                                        -- project description --
+                                    </div>
+                                    <span className="tag">React</span>
+                                    <span className="tag">Gatsby</span>
+                                    <span className="tag">GraphQL</span>
                                 </div>
-                                <span className="tag">Node.js</span>
-                                <span className="tag">Express</span>
-                                <span className="tag">MongoDB</span>
+                                <footer className="card-footer">
+                                    <a href="/" className="card-footer-item">
+                                        Live Demo
+                                    </a>
+                                    <a href="/" className="card-footer-item">
+                                        Source Code
+                                    </a>
+                                </footer>
                             </div>
-                            <footer className="card-footer">
-                                <a href="/" className="card-footer-item">
-                                    Live Demo
-                                </a>
-                                <a href="/" className="card-footer-item">
-                                    Source Code
-                                </a>
-                            </footer>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <div className={['card', projectStyles.card].join(' ')}>
-                            <header className="card-header">
-                                <p className="card-header-title">Project 2</p>
-                            </header>
-                            <div className="card-content">
-                                <div className="content">
-                                    {projectImgs[0]}
-                                    This is the description for Project 2.
-                                </div>
-                                <span className="tag">React</span>
-                                <span className="tag">Gatsby</span>
-                                <span className="tag">GraphQL</span>
-                            </div>
-                            <footer className="card-footer">
-                                <a href="/" className="card-footer-item">
-                                    Live Demo
-                                </a>
-                                <a href="/" className="card-footer-item">
-                                    Source Code
-                                </a>
-                            </footer>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
-};
+            </section>
+        );
+    }
+}
 
 export default Projects;
