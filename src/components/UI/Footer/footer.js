6d097704f939ebab1be4733179c3847eae4cafa4
alsonshareef_WSL <alson.shareef@gmail.com>
2020-01-26 19:46:41 +0800

    commenting footer out untill staticQuery syntax is updated

--- src/components/UI/Footer/footer.js
+++ src/components/UI/Footer/footer.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import footerStyles from './footer.module.scss';
+// import React from 'react';
+// import { graphql, useStaticQuery } from 'gatsby';
+// import footerStyles from './footer.module.scss';
 
-const footer = () => {
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    author
-                }
-            }
-        }
-    `);
+// const footer = () => {
+//     const data = useStaticQuery(graphql`
+//         query {
+//             site {
+//                 siteMetadata {
+//                     author
+//                 }
+//             }
+//         }
+//     `);
 
-    return (
-        <footer className={['footer', footerStyles.footer].join(' ')}>
-            <div className="content has-text-centered">
-                <p>{`Created by ${data.site.siteMetadata.author}, ©️ 2019`}</p>
-            </div>
-        </footer>
-    );
-};
+//     return (
+//         <footer className={['footer', footerStyles.footer].join(' ')}>
+//             <div className="content has-text-centered">
+//                 <p>{`Created by ${data.site.siteMetadata.author}, ©️ 2019`}</p>
+//             </div>
+//         </footer>
+//     );
+// };
 
-export default footer;
+// export default footer;
